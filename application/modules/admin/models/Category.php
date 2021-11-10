817276c5a425b54418a9e2a6d4bc350537149565
LE VAN THANG <l-thang@plott.local>
2021-11-10 17:04:43 +0700

    update-code

--- application/modules/admin/models/Category.php
+++ application/modules/admin/models/Category.php
@@ -24,6 +24,7 @@ class Admin_Model_Category extends Zend_Db_Table
 
   public function saveItem($arrParam = null, $option = null)
   {
+      $where = 'id = ' . $arrParam['id'];
     if ($option['task'] == 'category-add') {
       $row = $this->fetchNew();
       $row->name = $arrParam['name'];
@@ -32,7 +33,6 @@ class Admin_Model_Category extends Zend_Db_Table
     }
 
     if ($option['task'] == 'category-edit') {
-      $where = 'id = ' . $arrParam['id'];
       $row = $this->fetchRow($where);
       $row->name = $arrParam['name'];
       $row->status = $arrParam['status'];
@@ -40,7 +40,6 @@ class Admin_Model_Category extends Zend_Db_Table
     }
 
     if ($option['task'] == 'category-delete') {
-      $where = 'id = ' . $arrParam['id'];
       $row = $this->fetchRow($where);
       $row->updated_at = date('Y-m-d H:i:s');
       $row->status = 0;
@@ -48,11 +47,10 @@ class Admin_Model_Category extends Zend_Db_Table
     }
 
     if ($option['task'] == 'category-restore') {
-      $where = 'id = ' . $arrParam['id'];
-      $row = $this->fetchRow($where);
-      $row->updated_at = date('Y-m-d H:i:s');
-      $row->status = 1;
-      $row->save();
+        $row = $this->fetchRow($where);
+        $row->updated_at = date('Y-m-d H:i:s');
+        $row->status = (!$arrParam['status'])  ;
+        $row->save();
     }
   }
 
--- application/modules/user/controllers/IndexController.php
+++ application/modules/user/controllers/IndexController.php
@@ -3,13 +3,35 @@
 class IndexController extends Mylib_Controller_Action
 {
 
+    //Mảng tham số nhận được ở mỗi action;
+    protected $_arrParam;
+
+    //đường dẫn của controller
+    protected $_currentController;
+
+
+    //đường dẫn action
+    protected $_actionMain;
+
     public function init()
     {
+        //Mảng tham số nhận được ở mỗi action
+        $this->_arrParam = $this->_request->getParams();
+        //Đường dẫn controller
+        $this->_currentController = '/' . $this->_arrParam['module'] . '/product/';
+        //Đường dẫn của action chính
+        $this->_actionMain = '/' . $this->_arrParam['module'] . '/' . $this->_arrParam['controller'] . '/index';
+        //Truyền ra view
+        $this->view->arrParam = $this->_arrParam;
+        $this->view->currentController = $this->_currentController;
+        $this->view->actionMain = $this->_actionMain;
         $template_path = TEMPLATE_PATH . "/user/system";
         $this->loadTemplate($template_path, 'template.ini', 'template');
     }
 
     public function indexAction()
     {
+        $tblProduct = new User_Model_Product();
+        $this->view->Items = $tblProduct->getListItem(null, array('task' => 'product-list'));
     }
 }
